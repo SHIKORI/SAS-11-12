4564e6731b768d50b8bb618623b6fee343aa8be0
MILES FELICIANO <115053837+SHIKORI@users.noreply.github.com>
2022-10-19 01:11:15 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brrt_brrt_4
+{
+    internal class Class
+    {
+        public string item { get; set; }
+        public int quantity;
+        public string unit { get; set; }
+        private double price;
+        
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using brrt_brrt_4;
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Class myitm = new Class();
+        myitm.item = "Item: Ariel";
+        Console.WriteLine(myitm.item);
+
+        Console.WriteLine("Enter Quantity: ");
+        int quantity = Convert.ToInt16(Console.ReadLine());
+
+        Class myunit = new Class();
+        myunit.unit = "Unit: Sachet";
+        Console.WriteLine(myunit.unit);
+
+        Class prc = new Class();
+        prc.Price = 7.50;
+        Console.WriteLine("Price:" +prc.Price);
+
+        Console.WriteLine("Amount:" + quantity * prc.Price);
+
+
+
+    }
+       
+        
+}
